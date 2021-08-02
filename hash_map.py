80b70b8b38d9da56fb78f89b7887d0004c6ecf5e
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-08-02 05:37:27 +0100

    add basic structure

---
+++ hash_map.py
@@ -0,0 +1,12 @@
+# here is some helpful documentation: https://www.interviewcake.com/concept/java/hash-map
+# has maps are really similar to arrays, but instead of number indicies, hash maps can create their own
+# to avoid hash collisions, the array value can hold a pointer to a linked list
+
+# def __init__(self) - set the size of the array
+# def get_hash(self, key) - takes a key and generates an appropriate index to assign that key
+# def add(self, key, value) - adds a value to a key (calls the get_hash function)
+# def get(self, key) - gets a value (or values?) - calls get_hash
+# def delete(self, key) - delete a key 
+# def keys(self) - list all the keys
+
+
