0741ad6ca056e52a0b15dc54506e7967ea77cfbe
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-11-15 11:33:42 +0000

    latest

--- graph.py
+++ graph.py
@@ -10,12 +10,12 @@ class Edge(object):
         return str(self)
 
 # represents all nodes, even unconnected nodes
+# make sure pass in Nodes to the graph
 class Graph(object):
     def __init__(self, edges=[], nodes=[]):
         self.edges = edges
         self.nodes = nodes
-        # check that all edges are contained in the nodes list
-        # don't want to initialise a nodes list that doesn't contain all nodes
+        # check that all edges are contained in the nodes list - don't want to initialise a nodes list that doesn't contain all nodes
         for edge in edges:
             if edge.a in nodes and edge.b in nodes:
                 pass
@@ -25,11 +25,51 @@ class Graph(object):
     def __str__(self):
         return "Nodes = " + str(self.nodes) + " Edges = " + str(self.edges)
 
-    # h/w - complete heaps h/w (max heap and heap sort)
-    # h/w - read about the traversals 
-    # DONE - h/w - exception should say "graph constructed with edge xx, but no such node found" / or easier - nice exception msg
+class Node(object):
+    def __init__(self, key): # key is the identity of the node (like an id), not the value 
+        self.key = key
+    
+    def __str__(self):
+        return str(self.key)
+    
+    def __repr__(self):
+        return str(self)
+    
+# compute extra properties for each node:
+    # distance - number edges from start (or source) to given node
+    # previous - previous of each node
+    # visited boolean/colour - have we visited node already? (max's book uses three colours - do we need three options or is a boolean enough?)
+def breadth_first_search(graph, start): # start parameter sometimes called source
+    queue = []
+    # first loop - graph - go through all nodes in graph and set initial state
+    for node in graph.nodes:
+        node.distance = None # value that makes it obvious it's not been set
+        node.previous = None
+        node.visited = False 
+    # set special values for start node
+    start.distance = 0
+    start.previous = None
+    start.visited = True 
+    queue.append(start) # add start node to the queue
+    # second loop - do the search
+    # use the visited flag to check if we've already seen them before if already in the queue (so don't add them again)
+    while not queue:
+        node = queue.pop[0] # get first thing out of queue
+        # find nodes connected to given node
+        # add them to the queue
+        # set their properties
+        # what is prev?
+        # TODO: add visited property later
+        # Amina - use graph object to find all nodes connected to given node
+        # chat my findings to Max and then continue step by step
 
-myEdges = [Edge(1,2), Edge(2,3), Edge(3,1)]
+# h/w - in example, instead of Ints, use Kings X/Farringdon example
+# h/w - visualise the distance/prev/visited    
+# h/w - start looking at depth first search
+
+myNodes = [Node(1), Node(2), Node(3)]
+myEdges = [Edge(myNodes[0],myNodes[1]), Edge(myNodes[1],myNodes[2]), Edge(myNodes[2],myNodes[0])]
 print(myEdges)
-myGraph = Graph(myEdges, [1,2,3])
+myGraph = Graph(myEdges, myNodes)
 print(myGraph)
+breadth_first_search(myGraph, 1)
